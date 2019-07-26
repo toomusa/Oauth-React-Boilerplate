350d765ee06820dd38d28a09df9ad2358575c2f2
toomusa <3895499+toomusa@users.noreply.github.com>
2019-07-26 10:51:38 -0700

    email validation

--- models/User.js
+++ models/User.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const validator = require("validator");
+
+const validateEmail = email => {
+    return validator.isEmail(email);
+}
 
 const UserSchema = new Schema ({
     email: {
         type: String,
         unique: true,
         required: true,
-        lowercase: true
+        lowercase: true,
+        validate: [
+            validateEmail,
+            "Please enter a valid email address"
+        ]
     },
     password: {
         type: String,
