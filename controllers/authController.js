57e13ef9481c79cd71bdacc34d7132fcbf11ca05
toomusa <3895499+toomusa@users.noreply.github.com>
2019-07-26 10:39:28 -0700

    create user model and fix signup route

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+
+module.exports = {
+    signUp: async (req, res) => {
+        const { email, password } = req.body;
+
+        if (!email || !password) return res.status(422).json({error: "You must provide an email and password"});
+        
+        try {
+            // Check if the email already exists in db
+            const existingUser = await db.User.findOne({email});
+            // If user exists, throw error
+            if (existingUser) return res.status(422).json({error: "Email is in use"});
+            // Create new user object
+            const user = new db.User({ email, password });
+            // save user in db
+            await user.save();
+            // send user back
+            res.json(user);
+        } catch(e) {
+            res.status(404).json({e});
+        }
+}
+}
